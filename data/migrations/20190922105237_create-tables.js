67f1617faca30f991ffc3ead00bf7ab9fee04d51
astillo <clomes.alex@gmail.com>
2019-09-22 11:11:38 -0500

    migrations complete

---
+++ data/migrations/20190922105237_create-tables.js
@@ -0,0 +1,23 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('users', users => {
+        users.increments()
+        users.string('username', 255).notNullable().unique()
+        users.string('password', 255).notNullable()
+    })
+        .createTable('tasks', task => {
+            task.increments()
+            task.string('user_id', 128).notNullable().unsigned().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE')
+            task.string('title', 255).notNullable()
+            task.string('description', 255).notNullable()
+            task.string('segment', 128).notNullable()
+            task.string('due_by', 255).notNullable()
+            task.boolean('completed')
+            task.timestamp('created_at').defaultTo(knex.fn.now())
+        })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('tasks')
+        .dropTableIfExists('users')
+};
